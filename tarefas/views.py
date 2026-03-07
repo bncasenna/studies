b594b46eca2b7034b97787d2ea1e548ef49e8ca6
Uniruy <Uniruy@APL-WS-029247.EDUC.ACAD>
2026-03-07 11:20:08 -0300

    Atualizando código

--- tarefas/views.py
+++ tarefas/views.py
@@ -7,4 +7,28 @@ def home(request):
     completed_tasks = 0
     remaining_tasks = 0
     pct = 0
-    return render(request,'tarefas/home.html')
+    for task in tasks:
+        if task.done:
+            completed_tasks+=1
+
+    remaining_tasks = total_tasks = completed_tasks
+    if total_tasks >0:
+        pct = (completed_tasks/total_tasks)*100
+    return render(
+        request,
+        'tarefas/home.html',
+        {
+        'tasks':tasks,
+        'total':total_tasks,
+        'remaining':remaining_tasks,
+        'pct':pct
+        }
+    )
+    
+
+
+def add(request):
+    form=TaskForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('home')
