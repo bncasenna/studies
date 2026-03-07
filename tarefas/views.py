dcb6a5dca8a0c1426e19202e5f4300a25ef798d0
Uniruy <Uniruy@APL-WS-029247.EDUC.ACAD>
2026-03-07 10:36:44 -0300

    aiviade sabado

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render,redirect
-
+from .models import Task
+from .forms import TaskForm
 def home(request):
+    tasks = Task.objects.all()
+    total_tasks = len(tasks)
+    completed_tasks = 0
+    remaining_tasks = 0
+    pct = 0
     return render(request,'tarefas/home.html')
